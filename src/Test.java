37e61287633eefdc15f2850130cafd208a42a712
chuyouhong <chuyh@medishare.cn>
2017-01-17 10:04:17 +0800

    update

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,16 @@
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("2.0".compareTo("2.0.0"));
+		Double a=2.16;
+		Double b=Math.floor(a*10)/10;
+		
+		String bb=String.format("%.1f", Double.valueOf(a));
+		String bab=String.format("%.1f", Double.valueOf(b));
+		System.out.println(bb);
+		System.out.println(bab);
+		String aaa="622 8480660643815310";
+		System.out.println(aaa.replace(" ", ""));
+		
 	}
 
 }
